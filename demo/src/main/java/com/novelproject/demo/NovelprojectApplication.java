3e21503279c94ba76d3ad5a2930b8292604a4261
GwPark894 <pgw894@gmail.com>
2024-08-10 15:47:36 +0900

    Add files via upload

---
+++ demo/src/main/java/com/novelproject/demo/NovelprojectApplication.java
@@ -0,0 +1,13 @@
+package com.novelproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelprojectApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/novelproject/demo/NovelprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
